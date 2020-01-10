5b6f59b010b241aa16423abd19a5d00cd8ba4288
Wilberforce Zachary Ouma <wilalebit@yahoo.com>
2020-01-10 12:08:40 -0500

    Update gff2gtf.r

--- gff2gtf.r
+++ gff2gtf.r
@@ -4,6 +4,8 @@ library("rtracklayer")
 gff <- import("../reference/ITAG3.2_gene_models.gff")
 export(gff,"../reference/ITAG3.2_gene_models.gtf","gtf")
 
+#NOTE: ADD GTF.attrType="ID" argument in Rsubread::featureCounts function, then you don't need the bash command bellow!!!
+
 ##the following code uses bash commands:
 #then clean the output gff with:
 sed -i -e 's/ID=gene/gene_id/' ITAG3.2_gene_models.gtf
